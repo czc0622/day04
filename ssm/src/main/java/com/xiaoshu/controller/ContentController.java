83295a99315f446006d70b8993f0832a35bea79f
Caizicheng <940048487@qq.com>
2020-08-06 21:41:02 +0800

    day04作业

---
+++ ssm/src/main/java/com/xiaoshu/controller/ContentController.java
@@ -0,0 +1,176 @@
+package com.xiaoshu.controller;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.xiaoshu.config.util.ConfigUtil;
+import com.xiaoshu.entity.Category;
+import com.xiaoshu.entity.Content;
+import com.xiaoshu.entity.Operation;
+import com.xiaoshu.entity.Role;
+import com.xiaoshu.entity.User;
+import com.xiaoshu.service.ContentService;
+import com.xiaoshu.service.OperationService;
+import com.xiaoshu.service.RoleService;
+import com.xiaoshu.service.UserService;
+import com.xiaoshu.util.StringUtil;
+import com.xiaoshu.util.TimeUtil;
+import com.xiaoshu.util.WriterUtil;
+
+import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.PageInfo;
+
+@Controller
+@RequestMapping("content")
+public class ContentController extends LogController{
+	static Logger logger = Logger.getLogger(ContentController.class);
+
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private RoleService roleService ;
+	
+	@Autowired
+	private OperationService operationService;
+	
+	@Autowired
+	private ContentService cs;
+	
+	@RequestMapping("contentIndex")
+	public String index(HttpServletRequest request,Integer menuid) throws Exception{
+		List<Category> cglist = cs.findAllCategory();
+		List<Role> roleList = roleService.findRole(new Role());
+		List<Operation> operationList = operationService.findOperationIdsByMenuid(menuid);
+		request.setAttribute("operationList", operationList);
+		request.setAttribute("roleList", roleList);
+		request.setAttribute("cglist", cglist);
+		
+		return "content";
+	}
+	
+	
+	@RequestMapping(value="cList",method=RequestMethod.POST)
+	public void cList(Content content,HttpServletRequest request,HttpServletResponse response,String offset,String limit) throws Exception{
+		try {
+			
+			Integer pageSize = StringUtil.isEmpty(limit)?ConfigUtil.getPageSize():Integer.parseInt(limit);
+			Integer pageNum =  (Integer.parseInt(offset)/pageSize)+1;
+			PageInfo<Content> cList= cs.findContentPage(content,pageNum,pageSize);
+			JSONObject jsonObj = new JSONObject();
+			jsonObj.put("total",cList.getTotal() );
+			jsonObj.put("rows", cList.getList());
+	        WriterUtil.write(response,jsonObj.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("用户展示错误",e);
+			throw e;
+		}
+	}
+	
+	
+	// 新增或修改
+	@RequestMapping("reserveContent")
+	public void reserveContent(HttpServletRequest request,Content content,HttpServletResponse response){
+		Integer contentId = content.getContentid();
+		JSONObject result=new JSONObject();
+		try {
+			if (contentId != null) {   // userId不为空 说明是修改
+				Content contenttitle = cs.existFindByName(content.getContenttitle());
+				if(contenttitle==null){
+					String fomatTime = TimeUtil.formatTime(new Date(), "yyyy-MM-dd");
+					Date parseTime = TimeUtil.ParseTime(fomatTime, "yyyy-MM-dd");
+					content.setCreatetime(parseTime);
+					cs.updateContent(content);
+					result.put("success", true);
+				}
+				if(contenttitle != null && contenttitle.getContentid().compareTo(contentId)==0){
+					String fomatTime = TimeUtil.formatTime(new Date(), "yyyy-MM-dd");
+					Date parseTime = TimeUtil.ParseTime(fomatTime, "yyyy-MM-dd");
+					content.setCreatetime(parseTime);
+					cs.updateContent(content);
+					result.put("success", true);
+				}else{
+					result.put("success", true);
+					result.put("errorMsg", "该用户名被使用");
+				}
+				
+			}else {   // 添加
+				if(cs.existFindByName(content.getContenttitle())==null){  // 没有重复可以添加
+					String fomatTime = TimeUtil.formatTime(new Date(), "yyyy-MM-dd");
+					Date parseTime = TimeUtil.ParseTime(fomatTime, "yyyy-MM-dd");
+					content.setCreatetime(parseTime);
+					cs.addContent(content);
+					result.put("success", true);
+				} else {
+					result.put("success", true);
+					result.put("errorMsg", "该用户名被使用");
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("保存用户信息错误",e);
+			result.put("success", true);
+			result.put("errorMsg", "对不起，操作失败");
+		}
+		WriterUtil.write(response, result.toString());
+	}
+	
+	
+	@RequestMapping("deleteContent")
+	public void delUser(HttpServletRequest request,HttpServletResponse response){
+		JSONObject result=new JSONObject();
+		try {
+			String[] ids=request.getParameter("ids").split(",");
+			for (String id : ids) {
+				cs.deleteContent(Integer.parseInt(id));
+			}
+			result.put("success", true);
+			result.put("delNums", ids.length);
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("删除用户信息错误",e);
+			result.put("errorMsg", "对不起，删除失败");
+		}
+		WriterUtil.write(response, result.toString());
+	}
+	
+	@RequestMapping("editPassword")
+	public void editPassword(HttpServletRequest request,HttpServletResponse response){
+		JSONObject result=new JSONObject();
+		String oldpassword = request.getParameter("oldpassword");
+		String newpassword = request.getParameter("newpassword");
+		HttpSession session = request.getSession();
+		User currentUser = (User) session.getAttribute("currentUser");
+		if(currentUser.getPassword().equals(oldpassword)){
+			User user = new User();
+			user.setUserid(currentUser.getUserid());
+			user.setPassword(newpassword);
+			try {
+				userService.updateUser(user);
+				currentUser.setPassword(newpassword);
+				session.removeAttribute("currentUser"); 
+				session.setAttribute("currentUser", currentUser);
+				result.put("success", true);
+			} catch (Exception e) {
+				e.printStackTrace();
+				logger.error("修改密码错误",e);
+				result.put("errorMsg", "对不起，修改密码失败");
+			}
+		}else{
+			logger.error(currentUser.getUsername()+"修改密码时原密码输入错误！");
+			result.put("errorMsg", "对不起，原密码输入错误！");
+		}
+		WriterUtil.write(response, result.toString());
+	}
+}
